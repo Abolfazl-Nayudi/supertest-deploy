7caa7ac383032b22268839de03d66320599f96e4
Abolfazl Nayudi <abolfazl.nayudi@gmail.com>
2024-09-03 07:47:26 +0330

    edit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const { connectDB } = require("./db/connectDB");
+const { connect } = require("./db/connectDB");
 const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await connect(process.env.MONGO_URI);
     app.listen(PORT, () => {
       console.log("Server is running on port", PORT);
     });
